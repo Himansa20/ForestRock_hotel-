3b9fbbf6c580b412c17fc7a2ae7f8d82f560c77b
Himansa20 <138608513+Himansa20@users.noreply.github.com>
2024-07-14 23:49:46 +0530

    changes

--- js/script.js
+++ js/script.js
@@ -24,3 +24,27 @@ document.addEventListener('DOMContentLoaded', () => {
         }
     });
 });
+//carousel
+const buttons = document.querySelectorAll("[data-carousel-button]")
+
+buttons.forEach(button => {
+    button.addEventListener("click", () => {
+        moveToNextSlide(button.dataset.carouselButton === "next" ? 1 : -1)
+    })
+})
+
+function moveToNextSlide(offset) {
+    const slides = document.querySelector("[data-carousel] [data-slides]")
+    const activeSlide = slides.querySelector("[data-active]")
+    let newIndex = [...slides.children].indexOf(activeSlide) + offset
+    if (newIndex < 0) newIndex = slides.children.length - 1
+    if (newIndex >= slides.children.length) newIndex = 0
+
+    slides.children[newIndex].dataset.active = true
+    delete activeSlide.dataset.active
+}
+
+// Automatically change images every 3 seconds
+setInterval(() => {
+    moveToNextSlide(1)
+}, 10000)
