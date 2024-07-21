3269c30b457ecd5f01daba0b5459205adf5b056b
Himansa20 <138608513+Himansa20@users.noreply.github.com>
2024-07-21 11:22:03 +0530

    Landing page done

--- js/script.js
+++ js/script.js
@@ -64,3 +64,77 @@ overlay.addEventListener('click', function () {
     overlay.classList.add("hidden");
     certificate__img.classList.remove("show-certificate");
 })
+
+//Booking date function
+
+const dateSet = document.querySelectorAll(".datepicker-toggle-button");
+const dateInput = document.querySelectorAll('.datepicker-input');
+
+let x = dateInput.value;
+// Create a new Date object
+let currentDate = new Date();
+
+// Get the day of the month
+let day = currentDate.getDate();
+let nextDate = new Date(currentDate);
+nextDate.setDate(currentDate.getDate() + 1);
+let dayNext = nextDate.getDate();
+
+let month = currentDate.getMonth();
+
+let monthNames = [
+    "January", "February", "March", "April", "May", "June",
+    "July", "August", "September", "October", "November", "December"
+];
+let MonthName = monthNames[month];
+dateSet[0].textContent = `${day}${MonthName}`;
+dateSet[1].textContent = `${dayNext}${MonthName}`;
+/*
+dateInput.addEventListener('change', function () {
+    let cuurrent = document.querySelector(".current-date");
+    let BookingDate = dateInput.value;
+    cuurrent.textContent = BookingDate;
+})
+
+dateInput.addEventListener('change', function () {
+    let leave = document.querySelector(".leave-date");
+    let leaveDate = document.querySelector(".datepicker-input");
+    let BookingDate = leaveDate.value;
+    leave.textContent = BookingDate;
+})
+    */
+dateInput[0].addEventListener('change', function () {
+    let cuurrent = document.querySelector(".current-date");
+    let BookingDate = dateInput[0].value;
+    cuurrent.textContent = BookingDate;
+});
+dateInput[1].addEventListener('change', function () {
+    let leave_date = document.querySelector(".leave-date");
+    let BookingDate = dateInput[1].value;
+    leave_date.textContent = BookingDate;
+});
+/*
+const btnBook = document.querySelector(".navigation-button--primary");
+
+const dropbox = document.querySelector(".nav__bokking-dropdown");
+
+btnBook.addEventListener('click', function () {
+    let removecls = document.querySelector('.hide-booking');
+    removecls.classList.remove('hide-booking');
+})
+*/
+document.addEventListener('DOMContentLoaded', () => {
+    const bookNowBtn = document.getElementById('bookNowBtn');
+    const bookingDropdown = document.getElementById('bookingDropdown');
+
+    bookNowBtn.addEventListener('click', (event) => {
+        event.stopPropagation(); // Prevent the click event from propagating to the document
+        bookingDropdown.classList.toggle('hide-booking');
+    });
+
+    document.addEventListener('click', (event) => {
+        if (!bookingDropdown.contains(event.target) && !bookNowBtn.contains(event.target)) {
+            bookingDropdown.classList.add('hide-booking');
+        }
+    });
+});
