6aaa785c0e4ca1df30c78822761769954a9c0d4b
Himansa20 <138608513+Himansa20@users.noreply.github.com>
2024-09-01 23:57:17 -0600

    Gallery_half-done

--- js/script.js
+++ js/script.js
@@ -9,7 +9,24 @@
 //         });
 //     });
 // });
+// Galery ffunction............................
+const tabs = document.querySelectorAll('.tab-btn');
+const all_content = document.querySelectorAll('.gallery-grid');
 
+tabs.forEach((tab, index) => {
+    tab.addEventListener('click', (e) => {
+        tabs.forEach(tab => { tab.classList.remove('active') });
+        tab.classList.add('active');
+
+        let line = document.querySelector('.line');
+        line.style.width = e.target.offsetWidth + "px"
+        line.style.left = e.target.offsetLeft + "px"
+
+        all_content.forEach(content => { content.classList.remove('active') });
+        all_content[index].classList.add('active')
+    })
+
+})
 
 
 document.addEventListener('DOMContentLoaded', () => {
